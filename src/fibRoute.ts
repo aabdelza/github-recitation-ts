f7363fe5427c476045bdab1bc7f2494419e0f688
Abdulraouf <aabdelza@unix.qatar.cmu.edu>
2023-08-24 16:44:35 +0300

    errors fixed

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,20 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import { fibonacci } from "./fib"; // Use ES6 import syntax
+import { Request, Response } from 'express'; // Import types for Request and Response
 
-export default (req, res) => {
+export default (req: Request, res: Response) => { // Add type annotations for req and res
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN = fibonacci(parseInt(num, 10)); // Parse with radix 10
+  let result: string;
 
-  if (fibN < 0) {
+  if (fibN >= 0) {
+    result = `fibonacci(${num}) is ${fibN}`; // Correct result
+  } else {
     result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
 };
+
